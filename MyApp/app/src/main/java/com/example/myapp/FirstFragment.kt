386692ee39293b4d6b35cb4e6806890429d4114d
Thomas YRIARTE <thyriarte@a106imac06.local.isima.fr>
2022-01-11 17:44:10 +0100

    fin1 - random cats

--- MyApp/app/src/main/java/com/example/myapp/FirstFragment.kt
+++ MyApp/app/src/main/java/com/example/myapp/FirstFragment.kt
@@ -5,8 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.myapp.databinding.FragmentFirstBinding
+import android.graphics.BitmapFactory
+
+import android.graphics.Bitmap
+import androidx.lifecycle.lifecycleScope
+import com.squareup.picasso.Picasso
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.BufferedInputStream
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -14,10 +31,12 @@ import com.example.myapp.databinding.FragmentFirstBinding
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
+    private var imgUrl = ""
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -34,6 +53,28 @@ class FirstFragment : Fragment() {
 
         binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+
+        binding.shuffleButton.setOnClickListener{
+            val url = URL("https://api.thecatapi.com/v1/images/search")
+            lifecycleScope.launchWhenCreated {
+                withContext(Dispatchers.IO) {
+                    var txt = url.readText()
+                    txt = txt.subSequence(1, txt.lastIndex) as String
+                    val json = JSONObject(txt)
+                    imgUrl = json.getString("url")
+                    lifecycleScope.launch {
+                        Picasso.with(context)
+                            .load(imgUrl)
+                            .into(binding.imageView);
+                    }
+                }
+            }
+
+
+            //TODO : Method call should happen from the main thread; -> changer le lifecyclescope.
+            //"""[{"breeds":[],"id":"3kg","url":"https://cdn2.thecatapi.com/images/3kg.jpg","width":500,"height":346}]"""
+
         }
     }
 
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Android
+
+Main : 
+    Generate cat images from https://thecatapi.com
+    Add some to your favorites, with a name
+Second:
+    RecyclerView of your favorite cats.
